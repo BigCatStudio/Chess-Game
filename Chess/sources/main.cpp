7078426e724221149008a26197d63bec0b94ae59
Piotr Budzyński <piotr.bud01@wp.pl>
2022-07-03 17:08:09 +0200

    Restructured folders for QML files

--- Chess/sources/main.cpp
+++ Chess/sources/main.cpp
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
     qmlRegisterType<TileBack>("Cpp.Classes", 1.0, 0.0, "TileBack");
     qmlRegisterType<FigureBack>("Cpp.Classes", 1.0, 0.0, "FigureBack");
 
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    const QUrl url(QStringLiteral("qrc:/gui/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
         if (!obj && url == objUrl)
